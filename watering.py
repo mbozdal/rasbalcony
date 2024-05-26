79780c90569164395f288b613893759b21e2e0aa
mbozdal <mehmet-bozdal@hotmail.com>
2024-05-26 10:02:34 +0300

    Update watering.py
    watering is 120 seconds

--- watering.py
+++ watering.py
@@ -38,7 +38,7 @@ pumpD = Pin(21, Pin.OUT)
 #initial parameters
 last_watering_time = 0
 watering_period = 0*60*60 #hours * min * seconds
-water_time = 60 #active time of pump in seconds
+water_time = 120 #active time of pump in seconds
 rest_time = 60 #rest time between pumps
 
 pumps = [pumpA, pumpB, pumpC, pumpD]
